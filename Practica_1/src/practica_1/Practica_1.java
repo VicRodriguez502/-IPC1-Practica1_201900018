6a5583f7de3207e2a8d02d0a9a8c6f7a633b353d
VicRodriguez502 <3015506460101@ingenieria.usac.edu.gt>
2022-02-05 00:36:40 -0600

    Iniciando menu de juego

--- Practica_1/src/practica_1/Practica_1.java
+++ Practica_1/src/practica_1/Practica_1.java
@@ -6,13 +6,20 @@ import java.util.Scanner;
 public class Practica_1 {
 
     public static void main(String[] args) {
-        Scanner tusita = new Scanner(System.in);
-        Scanner reales = new Scanner(System.in);
         
+        //variables para el juego
         int num ;
-        String name;
+        String name; 
+        String grande;
+        int fila;
+        int columna;
+        int food;
+        int par;
+        int trap;
         
-        // Texto para menú del juego         
+        // Texto para menú del juego
+        do {
+        Scanner tusita = new Scanner(System.in);
         System.out.println("║BIENVENIDO AL JUEGO PACMAN║");
         System.out.println("⇉⇉⇉⇉《IPC1 - 2022》⇇⇇⇇⇇");
         System.out.println("≛≛≛≛≛≛≛≛≛≛≛≛≛≛≛≛≛≛≛≛≛≛");
@@ -20,24 +27,43 @@ public class Practica_1 {
         System.out.println("2.      TABLA DE POSICIONES");
         System.out.println("3.                    SALIR");
         System.out.println("≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡");
-        System.out.println("Ingesa una opción:");  
-        num = reales.nextInt();
+        System.out.print("Ingesa una opción:");  
+        num = tusita.nextInt();
         if(num == 1) { 
             //En este if se hara el menu del juego como el juego en si
             System.out.println("MENÚ DE JUEGO");
             System.out.println("≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡");
             System.out.println("¿Estás listo para vencer el juego?");
-            System.out.println("Ingrese su Nombre:");
-            name = tusita.nextLine();
+            System.out.print("Ingrese su Nombre:");
+            name = tusita.next(); 
+            System.out.println("Ingrese del tamaño del tablero [mxn]");
+            grande = tusita.next() ;
+            System.out.println("Ingresa Cantidad de comida [0-28]");
+            food = tusita.nextInt() ;
+            System.out.println("Ingrese cantidad de paredes [0-13]");
+            par = tusita.nextInt() ;
+            System.out.println("Ingrese cantidad de Trampas [0-10]");
+            trap = tusita.nextInt() ;
+            System.out.println("ღღღღღღღღღღღღღღღღღღღღღღღღღღღღღღ");
+            System.out.println("Ingrese su Nombre:"+name);
+            System.out.println("Ingrese del tamaño del tablero [mxn]:"+grande); 
+            System.out.println("Ingresa Cantidad de comida [0-28]:"+food);
+            System.out.println("Ingrese cantidad de paredes [0-13]:"+par);
+            System.out.println("Ingrese cantidad de Trampas [0-10]:"+trap);
+            System.out.println("ღღღღღღღღღღღღღღღღღღღღღღღღღღღღღღ");
         }
+        
         if(num == 2){
             //En este iff mostare la tabla de posiciones
             System.out.println("JUGADORES:") ;
+            
+            
         }
         if (num == 3){
             // Este iff es para salir a la pantalla inicial
             System.out.println("¡Gracias por jugar!");
-        }      
-    }
-    
+        } 
+    }while (num <= 3);System.out.println("Ingrese Opción Correcta");
+        
+    }      
 }
