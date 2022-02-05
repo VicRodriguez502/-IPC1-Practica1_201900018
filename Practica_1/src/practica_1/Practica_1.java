3efffcf939b9e18c6bdbf951aa972931d6207b95
VicRodriguez502 <3015506460101@ingenieria.usac.edu.gt>
2022-02-04 21:45:12 -0600

    Menu del juego

--- Practica_1/src/practica_1/Practica_1.java
+++ Practica_1/src/practica_1/Practica_1.java
@@ -1,20 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package practica_1;
 
-/**
- *
- * @author Victor Rodriguez
- */
+import java.util.Scanner;
+
 public class Practica_1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner tusita = new Scanner(System.in);
+        Scanner reales = new Scanner(System.in);
+        
+        int num ;
+        String name;
+        
+        // Texto para menú del juego         
+        System.out.println("║BIENVENIDO AL JUEGO PACMAN║");
+        System.out.println("⇉⇉⇉⇉《IPC1 - 2022》⇇⇇⇇⇇");
+        System.out.println("≛≛≛≛≛≛≛≛≛≛≛≛≛≛≛≛≛≛≛≛≛≛");
+        System.out.println("1.            INICIAR JUEGO");
+        System.out.println("2.      TABLA DE POSICIONES");
+        System.out.println("3.                    SALIR");
+        System.out.println("≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡");
+        System.out.println("Ingesa una opción:");  
+        num = reales.nextInt();
+        if(num == 1) { 
+            //En este if se hara el menu del juego como el juego en si
+            System.out.println("MENÚ DE JUEGO");
+            System.out.println("≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡");
+            System.out.println("¿Estás listo para vencer el juego?");
+            System.out.println("Ingrese su Nombre:");
+            name = tusita.nextLine();
+        }
+        if(num == 2){
+            //En este iff mostare la tabla de posiciones
+            System.out.println("JUGADORES:") ;
+        }
+        if (num == 3){
+            // Este iff es para salir a la pantalla inicial
+            System.out.println("¡Gracias por jugar!");
+        }      
     }
     
 }
